412c2a0235f93acea9a63751ee772bacf0242998
PasitB06 <6733167521@student.chula.ac.th>
2025-03-23 17:00:10 +0700

    profile in menu

--- src/components/ClientTopMenu.tsx
+++ src/components/ClientTopMenu.tsx
@@ -1,11 +1,28 @@
 'use client'
 // ./src/components/ClientTopMenu.tsx
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import getUserProfile from "@/libs/getUserProfile";
 
-export default function ClientTopMenu({ session }: { session: any }) {
+export default function ClientTopMenu() {
   const [menuOpen, setMenuOpen] = useState(false);
+  
+  const { data: session } = useSession();
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (session?.user?.token) {
+        const userData = await getUserProfile(session.user.token);
+        setUser(userData);
+      }
+    };
+
+    fetchUser();
+  }, [session]);
 
   return (
     <div className="fixed top-0 left-0 right-0 w-screen h-[64px] z-30 bg-white border-b border-gray-400 flex items-center justify-between px-6 shadow-lg">
@@ -23,8 +40,9 @@ export default function ClientTopMenu({ session }: { session: any }) {
         </Link>
       </div>
 
-      {/* Desktop Navigation Links */}
       <div className="block flex">
+        
+
       <button 
           onClick={() => setMenuOpen(!menuOpen)}
           className="p-4 bg-white rounded"
@@ -38,17 +56,50 @@ export default function ClientTopMenu({ session }: { session: any }) {
       {/* Mobile Menu Overlay */}
       {menuOpen && (
         <div className="absolute top-[64px] right-0 w-96 h-screen bg-white shadow-lg z-40">
+          <div className="p-4 border-b flex items-center space-x-4">
+            <Image 
+              src={'/img/user.png'}
+              alt="user"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="h-[60px] w-auto"
+              />
+            <div>
+              {user ? 
+                <div>Name: {user.data.name}</div> 
+                : 
+                <div>Guest</div>
+              }
+              {user ? 
+                <div>Email: {user.data.name}</div> 
+                : 
+                <div></div>
+              }
+            </div>
+          </div>
+
+
           <Link href={'/restaurant'} onClick={() => setMenuOpen(false)}>
             <div className="p-4 border-b hover:bg-gray-100">Restaurant</div>
           </Link>
-          <Link href={'/myReservation'} onClick={() => setMenuOpen(false)}>
-            <div className="p-4 border-b hover:bg-gray-100">My Reservation</div>
-          </Link>
 
           {session ? 
-            <Link href={'/api/auth/signout'} onClick={() => setMenuOpen(false)}>
-              <div className="p-4 border-b hover:bg-gray-100">Sign-Out {session.user?.name}</div>
-            </Link> : (
+            <>
+              {user.data.role === 'admin' ?
+                <Link href={'/myReservation'} onClick={() => setMenuOpen(false)}>
+                  <div className="p-4 border-b hover:bg-gray-100">All Reservation</div>
+                </Link>
+                :
+                <Link href={'/myReservation'} onClick={() => setMenuOpen(false)}>
+                  <div className="p-4 border-b hover:bg-gray-100">My Reservation</div>
+                </Link>
+              }
+              <Link href={'/api/auth/signout'} onClick={() => setMenuOpen(false)}>
+                <div className="p-4 border-b hover:bg-gray-100">Sign-Out</div>
+              </Link>
+            </> 
+            : (
             <>
               <Link href={'/api/auth/signin'} onClick={() => setMenuOpen(false)}>
                 <div className="p-4 border-b hover:bg-gray-100">Sign-In</div>
