804700ab80d8dd8e8b716c9aeac02af57893ce63
CLOUD <6733288821@student.chula.ac.th>
2025-03-23 18:11:22 +0700

    something wrong

--- src/libs/addReservation.tsx
+++ src/libs/addReservation.tsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 44847b8 (fix reser adddd)
 export default async function addReservation(
   restaurantID: string,
   reservationData: Object,
   token: string
 ) {
-<<<<<<< HEAD
-
-=======
->>>>>>> 44847b8 (fix reser adddd)
   try {
     const res = await fetch(
       `http://localhost:5000/api/v1/restaurants/${restaurantID}/reservation`,
@@ -18,11 +11,8 @@ export default async function addReservation(
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-<<<<<<< HEAD
           Authorization: `Bearer ${token}`,
-=======
-          Authorization: `Bearer ${token}`, // Use the token passed as parameter
->>>>>>> 44847b8 (fix reser adddd)
+
         },
         body: JSON.stringify(reservationData),
       }
