b3db3ff0bf16a4277919df99a23085dae37c548a
kurominarkmak <pisitpat8189@gmail.com>
2025-03-24 14:17:59 +0700

    add lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4A90E2",
+      },
+    },
   },
   plugins: [],
-}
-
+};
